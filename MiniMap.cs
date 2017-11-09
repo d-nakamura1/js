bec2d1f8ce9c68777b1d2eda80eb562a9e0ac7fa
TTCAD\d-nakamura1 <d-nakamura@ttc-net.co.jp>
2017-11-09 14:07:12 +0900

    gmoni

---
+++ MiniMap.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniMap : MonoBehaviour {
+
+    RawImage mapGuiTexture;
+    private float intervalTime = 0.0f;
+    private int width = 300;
+    private int height = 200;
+    private double longitude;
+    private double latitude;
+    private int zoom = 16;
+    void Start()
+    {
+        mapGuiTexture = this.GetComponent<RawImage>();
+        GetPos();
+        GetMap();
+    }
+    void Update()
+    {
+        //毎フレーム読んでると処理が重くなるので、3秒毎に更新
+        intervalTime += Time.deltaTime;
+        if (intervalTime >= 3.0f)
+        {
+            GetPos();
+            GetMap();
+            intervalTime = 0.0f;
+        }
+    }
+    void GetPos()
+    {
+        //GPSで取得した緯度経度を変数に代入
+        StartCoroutine(GetGPS());
+        longitude = Input.location.lastData.longitude;
+        latitude = Input.location.lastData.latitude;
+        Debug.Log("sahyooooo : " + longitude + " : " + latitude);
+    }
+    void GetMap()
+    {
+        //マップを取得
+        StartCoroutine(GetStreetViewImage(latitude, longitude, zoom));
+        //StartCoroutine(GetStreetViewImage(35.658581, 139.745433, zoom));
+    }
+    private IEnumerator GetGPS()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            yield break;
+        }
+        Input.location.Start();
+        int maxWait = 120;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+        if (maxWait < 1)
+        {
+            print("Timed out");
+            yield break;
+        }
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            print("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            print("Location: " +
+                  Input.location.lastData.latitude + " " +
+                  Input.location.lastData.longitude + " " +
+                  Input.location.lastData.altitude + " " +
+                  Input.location.lastData.horizontalAccuracy + " " +
+                  Input.location.lastData.timestamp);
+        }
+        Input.location.Stop();
+    }
+    private IEnumerator GetStreetViewImage(double latitude, double longitude, double zoom)
+    {
+        //現在地マーカーはここの「&markers」以下で編集可能
+        string url = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=" + zoom + "&size=" + width + "x" + height + "&markers=size:mid%7Ccolor:red%7C" + latitude + "," + longitude;
+        WWW www = new WWW(url);
+        yield return www;
+        //マップの画像をTextureとして貼り付ける
+        mapGuiTexture.texture = www.texture;
+        //ミニマップに透明度を与える
+        mapGuiTexture.color = new Color(mapGuiTexture.color.r, mapGuiTexture.color.g, mapGuiTexture.color.b, 0.4f);
+    }
+}
---
+++ SpText.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpText : MonoBehaviour
+{
+
+    private float intervalTime = 0.0f;
+    private int printCount = 0;
+    private float updateTime = 0.25f;
+    private float maxX;
+    private float maxY;
+    private float maxZ;
+    private float minX;
+    private float minY;
+    private float minZ;
+    private string state = "";
+    public Text spText;
+
+    // Use this for initialization
+    void Start()
+    {
+        spText = this.GetComponent<Text>();
+        this.SetSpeed("");
+        ResetMinMax();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        intervalTime += Time.deltaTime;
+        if (intervalTime >= updateTime)
+        {
+            // 加速度センサの値を取得
+            Vector3 val = Input.acceleration;
+            Debug.Log("kasokuuuuuuuuuuuuu" + val);
+            //string kasoku = val.ToString();
+
+            if(val.x > maxX)
+            {
+                maxX = val.x;
+            }
+            else if(val.x < minX)
+            {
+                minX = val.x;
+            }
+
+            if (val.y > maxY)
+            {
+                maxY = val.y;
+            }
+            else if (val.y < minY)
+            {
+                minY = val.y;
+            }
+
+            if (val.z > maxZ)
+            {
+                maxZ = val.z;
+            }
+            else if (val.z < minZ)
+            {
+                minZ = val.z;
+            }
+
+            printCount++;
+
+            if(printCount >= 4)
+            {
+                if (maxZ >= 0.1 && maxZ < 1.0)
+                {
+                    state = "歩き";
+                }
+                else if (maxZ >= 1.0)
+                {
+                    if(maxY - minY >= 1.0)
+                    {
+                        state = "走り";
+                    }
+                    else
+                    {
+                        state = "車移動";
+                    }
+                }
+                else
+                {
+                    state = "止まっている？";
+                }
+                this.SetSpeed(state);
+                ResetMinMax();
+                printCount = 0;
+            }
+
+            intervalTime = 0.0f;
+        }
+        
+    }
+
+    public void SetSpeed(string speed)
+    {
+        spText.text = speed;
+    }
+
+    private void ResetMinMax()
+    {
+        maxX = 0.0f;
+        maxY = -1.0f;
+        maxZ = 0.0f;
+        minX = 0.0f;
+        minY = -1.0f;
+        minZ = 0.0f;
+    }
+}
